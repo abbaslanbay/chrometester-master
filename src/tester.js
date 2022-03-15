92a9cfd09a960d0175fd598540d420324fdde1d4
abbaslanbay <abbaslanbay@gmail.com>
2022-03-15 16:16:59 +0300

    push

--- src/tester.js
+++ src/tester.js
@@ -9,7 +9,7 @@ const users = require('./users');
  
     console.log(users.length)
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       devtools: true,
       // dumpio: true,
       args: [
